4896f56d0ddefee39f53f11b1de61305029b6a8f
Aryan Ghosh <aryan.ghosh.tentue@gmail.com>
2026-03-08 00:57:23 +0530

    partners and announcements

--- client/constants/partners.ts
+++ client/constants/partners.ts
@@ -1,61 +1,117 @@
 import { Partner } from "@/types/interface";
 
 export const partners: Partner[] = [
-    {
-        id: "1",
-        partner_name: "GDG on Campus FIEM",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567363/gdg_bdympb.png",
-        website_url:
-            "https://gdg.community.dev/gdg-on-campus-future-institute-of-engineering-management-kolkata-india/",
-    },
-    {
-        id: "2",
-        partner_name: "Byte Brigade",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/bb_jkg43i.png",
-        website_url: "https://bytebrigade.vercel.app/",
-    },
-    {
-        id: "3",
-        partner_name: "InnovateX Engineers Community",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/innovate_su34ws.jpg",
-        website_url: "https://luma.com/user/innovatex_com",
-    },
-    {
-        id: "4",
-        partner_name: "FIEM ACM Student Chapter",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/acm_fiem_awwjsp.png",
-        website_url: "https://www.fiem.acm.org/",
-    },
-    {
-        id: "5",
-        partner_name: "Hacktropica",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/hacktropica_reaqlu.png",
-        website_url: "https://hacktropica-new1.vercel.app/",
-    },
-    {
-        id: "6",
-        partner_name: "Flutter Kolkata",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/flutter_qbqwes.png",
-        website_url: "https://in.linkedin.com/company/flutter-kolkata",
-    },
-    {
-        id: "7",
-        partner_name: "AWS User Group Kolkata",
-        partner_type: "Community Partner",
-        logo_url:
-            "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772631549/aws_j1xvg6.png",
-        website_url: "https://www.linkedin.com/company/awsugkol/",
-    },
+  {
+    id: "1",
+    partner_name: "GDG on Campus FIEM",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567363/gdg_bdympb.png",
+    website_url:
+      "https://gdg.community.dev/gdg-on-campus-future-institute-of-engineering-management-kolkata-india/",
+  },
+  {
+    id: "2",
+    partner_name: "Byte Brigade",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/bb_jkg43i.png",
+    website_url: "https://bytebrigade.vercel.app/",
+  },
+  {
+    id: "3",
+    partner_name: "InnovateX Engineers Community",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/innovate_su34ws.jpg",
+    website_url: "https://luma.com/user/innovatex_com",
+  },
+  {
+    id: "4",
+    partner_name: "FIEM ACM Student Chapter",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/acm_fiem_awwjsp.png",
+    website_url: "https://www.fiem.acm.org/",
+  },
+  {
+    id: "5",
+    partner_name: "Flutter Kolkata",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772567364/flutter_qbqwes.png",
+    website_url: "https://in.linkedin.com/company/flutter-kolkata",
+  },
+  {
+    id: "6",
+    partner_name: "AWS User Group Kolkata",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772631549/aws_j1xvg6.png",
+    website_url: "https://www.linkedin.com/company/awsugkol/",
+  },
+  {
+    id: "7",
+    partner_name: "Technical Community Universe",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772907300/tcu_zqsscy.jpg",
+    website_url: "https://linktr.ee/technicalcommunityuniverse",
+  },
+  {
+    id: "8",
+    partner_name: "Kolkata Oxford Debating Circuit",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772907300/kodc_ctjgdi.jpg",
+    website_url: "https://www.instagram.com/kolkataoxforddc?igsh=MWVrcWptdHZrNW95cQ==",
+  },
+  {
+    id: "9",
+    partner_name: "Calcutta Musings",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772907300/Calcutta_Musings_Full_HD_Logo_jnhi6t.png",
+    website_url: "https://www.instagram.com/musingscommunity?igsh=MTN1YXEyeW9rN2Zneg%3D%3D",
+  },
+  {
+    id: "10",
+    partner_name: "GDG on Campus JIS University",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772909842/LOGOS_zbzcub.png",
+    website_url: "https://gdg.community.dev/gdg-on-campus-jis-university-kolkata-india/",
+  },
+  {
+    id: "11",
+    partner_name: "IEI Student Chapter ECE",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772909644/LOGO_SCECE_-_Abhinandan_Kumar_Pandey_jk13sk.png",
+    website_url: "https://sceceaot.com",
+  },
+  {
+    id: "12",
+    partner_name: "Hacknfinity",
+    partner_type: "Community Partner",
+    logo_url:
+      "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772909734/IMG-20260102-WA0074_5_-_Abhinav_Kumar_judccv.jpg",
+    website_url: "https://chat.whatsapp.com/Gu0nGT4Up1Y2W1RlrF7Dpn",
+  },
+  // {
+  //   id: "13",
+  //   partner_name: "CodeRush X",
+  //   partner_type: "Community Partner",
+  //   logo_url:
+  //     "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772911511/BG_Remove_CodeRush_X_Logo_MAIN_-_CodeRushX_vefhbe.png",
+  //   website_url: "https://www.linkedin.com/company/coderushx-community",
+  // },
+  // {
+  //   id: "14",
+  //   partner_name: "GDG on Campus TMSL",
+  //   partner_type: "Community Partner",
+  //   logo_url:
+  //     "https://res.cloudinary.com/dnil7l0j1/image/upload/v1772909842/LOGOS_zbzcub.png",
+  //   website_url: "https://gdg.community.dev/gdg-on-campus-techno-main-salt-lake-kolkata-india/",
+  // },
 ];
