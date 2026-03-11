3d57d17d8eef0852745afda1762a64f95c17ce0f
asengupta07 <arnabsengupta104@gmail.com>
2026-03-12 04:27:17 +0530

    unstop

--- client/constants/unstop.ts
+++ client/constants/unstop.ts
@@ -3,6 +3,6 @@ export const UNSTOP_LINKS: Record<string, string> = {
   //   "https://unstop.com/o/PK1iT4I?lb=fZenmMA&utm_medium=Share&utm_source=mohitjen7297&utm_campaign=Online_coding_challenge",
   // "Skeld Sprint":
   //   "https://unstop.com/o/LbE405U?lb=rKbIzk8u&utm_medium=Share&utm_source=online_coding_challenge&utm_campaign=AndsD27HIT",
-  ModelForge:
-    "https://unstop.com/o/FztuXqn?lb=6XtMchPk&utm_medium=Share&utm_source=126alraj9688&utm_campaign=Online_coding_challenge",
+  // ModelForge:
+  //   "https://unstop.com/o/FztuXqn?lb=6XtMchPk&utm_medium=Share&utm_source=126alraj9688&utm_campaign=Online_coding_challenge",
 };
